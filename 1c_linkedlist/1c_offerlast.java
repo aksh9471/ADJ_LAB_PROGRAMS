39b0ade150b6b26a1225363ee33145469444b30f
aksh9471 <hegdeakshay517@gmail.com>
2025-05-22 14:00:04 +0530

    Create 1c_offerlast.java

---
+++ 1c_linkedlist/1c_offerlast.java
@@ -0,0 +1,19 @@
+Write a Java program to insert the specified element at the end of a linked list.( using
+l_listobj.offerLast("Pink"))?
+  code:
+  package list1;
+import java.util.LinkedList;
+public class e8 {
+public static void main(String []args) {
+	LinkedList<String> l3  = new LinkedList<>();
+	l3.add("Red");
+	l3.add("green");
+	l3.add("Blue");
+	l3.add("violet");
+	l3.add("Orange");
+	l3.add("white");
+	//adding last ele
+	l3.offerLast("pink");
+	System.out.println("After adding last: "+l3);
+}
+}
