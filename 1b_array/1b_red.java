c474fa47af0f27c195ee9fbac213a220acd68c35
aksh9471 <hegdeakshay517@gmail.com>
2025-05-22 13:55:26 +0530

    Create 1b_red.java

---
+++ 1b_array/1b_red.java
@@ -0,0 +1,17 @@
+1. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not?
+  code:
+package list1;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class e1 {
+	public static void main(String []args) {
+		ArrayList<String> array1 = new ArrayList<>();
+		//adding elements
+		array1.addAll(Arrays.asList("Red","Orange","green","blue"));
+		//searching red is there are not
+		boolean element = array1.contains("Red");
+		System.out.print("red is there or not ?"+element);
+	}
+}
