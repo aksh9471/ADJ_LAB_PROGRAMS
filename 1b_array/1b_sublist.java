d29d0ab16d56d3530948d749aebb1b767adab3c1
aksh9471 <hegdeakshay517@gmail.com>
2025-05-22 13:57:10 +0530

    Create 1b_sublist.java

---
+++ 1b_array/1b_sublist.java
@@ -0,0 +1,17 @@
+Write a java program for getting different colors through ArrayList interface and extract the
+elements 1st and 2nd from the ArrayList object by using SubList()?
+  code:
+  package list1;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class e4 {
+public static void main(String []args) {
+	ArrayList<String> array4 = new ArrayList<>();
+	//adding elements
+		array4.addAll(Arrays.asList("Red","Orange","green","violet","white"));
+		//sublist
+		List<String> sublist =  array4.subList(0,2);
+		System.out.println("sublist of 1st and 2nd: "+sublist);
+}
+}
